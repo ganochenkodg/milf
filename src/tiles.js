a66ea877361359c4df8587a1f1f0b121f6f0b4f1
Dmitrii Ganochenko <dmitriig@google.com>
2025-02-15 18:29:47 +0100

    add terrain

--- src/tiles.js
+++ src/tiles.js
@@ -1,8 +1,9 @@
 var tileSet = document.createElement('img');
 tileSet.src = 'resources/tiles.png';
-var terrains = ['dungeon', 'stone'];
-var playerTiles = ['human', 'dwarf', 'wizard'];
+var terrains = ['dungeon', 'stone', 'maze'];
+var playerTiles = ['human', 'dwarf', 'wizard', 'wizard2', 'thief', 'knight'];
 var gameTilemap = {
+  //dungeon tiles
   dungeonwall: [0, 0],
   dungeonwallhor: [32, 0],
   dungeonfloor: [64, 0],
@@ -19,7 +20,19 @@ var gameTilemap = {
   stonedooropen: [160, 32],
   stonestairdown: [192, 32],
   stonestairup: [224, 32],
+  mazewall: [0, 64],
+  mazewallhor: [32, 64],
+  mazefloor: [64, 64],
+  mazefloorrandom: [96, 64],
+  mazedoorclose: [128, 64],
+  mazedooropen: [160, 64],
+  mazestairdown: [192, 64],
+  mazestairup: [224, 64],
+  //player tilkes
   human: [288, 0],
   dwarf: [256, 0],
-  wizard: [320, 0]
+  wizard: [320, 0],
+  wizard2: [352, 0],
+  thief: [384, 0],
+  knight: [420, 0]
 };
